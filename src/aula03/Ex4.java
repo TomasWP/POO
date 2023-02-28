38b15cbc1d95d4892fa2372eb47b2cad329c1d12
TomasWP <tomasfonseca14@ua.pt>
2023-02-28 20:24:17 +0000

    ficheiros POO

--- src/aula03/Ex4.java
+++ src/aula03/Ex4.java
@@ -20,9 +20,9 @@ public class Ex4 {
                     notas [i][j] = Math.random()*20;
                 }
             }
-            System.out.printf(" %1s    %1s    %s","NotaT","NotaP","Pauta\n");
+            System.out.printf("%5s %7s %7s\n","NotaT","NotaP","Pauta\n");
             for (int i = 0; i<notas[0].length; i++){
-                System.out.printf("  %.1f      %.1f      %d\n",notas[0][i],notas[1][i],UserInput.nota_final(notas[0][i], notas[1][i]));
+                System.out.printf("%5.1f %7.1f %6d\n",notas[0][i],notas[1][i],UserInput.nota_final(notas[0][i], notas[1][i]));
             }
         }catch(Exception e){
             System.out.println("Insira um valor inteiro!");
