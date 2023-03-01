e1744778707e3277d442e5ead1ee968cd938ebf2
TomasWP <tomasfonseca14@ua.pt>
2023-03-01 12:37:34 +0000

    ficheiros POO

--- src/aula03/Ex3.java
+++ src/aula03/Ex3.java
@@ -5,7 +5,7 @@ import Util.UserInput;
 public class Ex3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int random_number, number;
+        int random_number, number, counter=0;
         String escolha = "s";
         try{
             random_number = (int) (Math.random()*101)+1;
@@ -14,12 +14,13 @@ public class Ex3 {
                     do{
                         number = sc.nextInt();
                     }while (UserInput.positivo(number) || !UserInput.in_between(number, 1, 100));
+                    counter += 1;
                     if (number>random_number){
                         System.out.print("Alto! ");
                     }else if(number<random_number){
                         System.out.print("Baixo! ");
                     }else{
-                        System.out.println("Acertou! Se pretende continuar insira S ou Sim!");
+                        System.out.println("Acertou com "+counter+" tentativas! Se pretende continuar insira S ou Sim!");
                         escolha = sc.next();
                     }
             }while(escolha.equalsIgnoreCase("s") || escolha.equalsIgnoreCase("sim"));
